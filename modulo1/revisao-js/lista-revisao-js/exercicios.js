8110d79fb4643b7cd1fcde168ae18c03d70f9874
MaritoAmbrosio <mario_conduta@hotmail.com>
2022-03-17 21:39:05 -0300

    Revisão JS Ex04

--- modulo1/revisao-js/lista-revisao-js/exercicios.js
+++ modulo1/revisao-js/lista-revisao-js/exercicios.js
@@ -23,7 +23,13 @@ function retornaArrayOrdenado(array) {
 
 // EXERCÍCIO 04
 function retornaNumerosPares(array) {
-
+    const numerosPares = (valor1) => {
+        if (valor1 % 2 === 0) {
+            return valor1
+        }
+    }
+    const filtrarPares = array.filter(numerosPares)
+    return filtrarPares
 }
 
 // EXERCÍCIO 05
