d766b7018bd169a578da2b3473094895efbe4b5d
MaritoAmbrosio <mario_conduta@hotmail.com>
2022-06-03 18:28:28 -0300

    Use-Effect completo

--- modulo4/use-effect/src/App.js
+++ modulo4/use-effect/src/App.js
@@ -1,32 +1,36 @@
-import './App.css';
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios'
+import './App.css';
+import PokeCard from './PokeCard/PokeCard';
 
-function App() {
-  const {pokeList, setPokeList} = useState([])
-  const {pokeName, setPokeName} = useState("")
+export default function App() {
+  const [PokeList, setPokeList] = useState([])
+  const [pokeName, setPokeName] = useState("")
 
   useEffect(() => {
     axios.get("https://pokeapi.co/api/v2/pokemon/?limit=151")
-    .then((resp)=>{    
-      setPokeList(resp.data.results)
-    })
-    .catch((err)=>{
-      console.log(err.data)
-    })
+      .then((res) => {
+        setPokeList(res.data.results)
+      })
+      .catch(() => {
+      })
   }, [])
 
-    console.log(pokeList)
+  const changePokeName = (e) => {
+    setPokeName(e.target.value)
+  }
+
   return (
     <>
-
-  <select>
-    <option value="">Nenhum</option>
-
-  </select>
-
+      <select onChange={changePokeName}>
+        <option value="">Nenhum</option>
+        {PokeList.map((pokemon) => {
+          return (
+            <option key={pokemon.name} value={pokemon.name}>{pokemon.name}</option>
+          )
+        })}
+      </select>
+      {pokeName && <PokeCard  pokemon={pokeName}/>}
     </>
   );
 }
-
-export default App;
