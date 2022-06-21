da5f198c9ae884945ee11718d437fe267b11f3fc
MaritoAmbrosio <mario_conduta@hotmail.com>
2022-06-21 01:00:12 -0300

    Astromatch atualizado

--- modulo4/astromatch/src/App.js
+++ modulo4/astromatch/src/App.js
@@ -7,6 +7,16 @@ import axios from 'axios';
 export default function App() {
 const usuario = "Mario"
 const [arrayDeUsuarios, setArrayDeUsuarios] = useState([])
+const [arrayDeMatch, setArrayDeMatch] = useState([
+  {
+    id: "2LAsTKPOVs3tavH6ga6v",
+    photo: "https://static.ndmais.com.br/2019/07/P-15-Texto-1-Foto-1-Anitta-533x800.jpg",
+    name: "Anitta",
+    age: 26,
+    bio: "Amo cachorros e sair para dançar. Procuro alguém animado e sem neuras.",
+    photo_alt: "Morena, cabelo longo castanho liso, vestindo blusa e calça branca. Ela se encontra em pé de braços cruzados em frente a um fundo cinza."
+}
+])
 
 const PegarMatches = (aluno) => {
   axios.get(`https://us-central1-missao-newton.cloudfunctions.net/astroMatch/:${aluno}/person`)
@@ -18,16 +28,54 @@ const PegarMatches = (aluno) => {
   })
 }
 
+const MatchTodos = () => {
+  axios.get(`https://us-central1-missao-newton.cloudfunctions.net/astroMatch/:${usuario}/matches`)
+  .then((response)=>{
+    setArrayDeMatch(response.data.profile)
+  })
+  .catch((err)=>{
+    console.log(err)
+  })
+}
+
+const DarMatch = () => {
+  const body = {
+    id: arrayDeUsuarios.id,
+    choice: true
+  }
+
+  axios.post(`https://us-central1-missao-newton.cloudfunctions.net/astroMatch/:${usuario}/choose-person`, body)
+}
+
+const DarDesMatch = () => {
+  const body = {
+    id: arrayDeUsuarios.id,
+    choice: false
+  }
+
+  axios.post(`https://us-central1-missao-newton.cloudfunctions.net/astroMatch/:${usuario}/choose-person`, body)
+}
 useEffect(()=>{
   PegarMatches(usuario)
 },[])
 
+
+
   return (
     <>
       <div className="Pagina">
-        <Menu />
+        <Menu 
+        ListaDeMatch={arrayDeMatch}
+        usuario={usuario}
+        />
         <Layout
          imagem={arrayDeUsuarios.photo}
+         bio={arrayDeUsuarios.bio}
+         age={arrayDeUsuarios.age}
+         name={arrayDeUsuarios.name}
+
+         DarMatch={DarMatch}
+         DarDesMatch= {DarDesMatch}
          />
       </div>
     </>
--- modulo4/astromatch/src/Components/Menu.js
+++ modulo4/astromatch/src/Components/Menu.js
@@ -1,15 +1,35 @@
-import React, {useState, useEffect} from "react"
+import axios from "axios"
+import React, { useState, useEffect } from "react"
 
-function Menu() {
+function Menu(props) {
+
+  useEffect(()=>{
+    axios.get(`https://us-central1-missao-newton.cloudfunctions.net/astroMatch/:${props.usuario}/matches`)
+    .then((response)=>{
+      props.setArrayDeMatch(response.data.profile)
+    })
+    .catch((err)=>{
+      console.log(err)
+    })
+  },[])
+
   return (
     <>
-    <div className="Menu">
-      <div className="Funcionalidades">
-        <button className="BotaoMenu">USUARIO</button>
-        <button className="BotaoMenu">CONTAS CURTIDAS</button>
-      </div>
+      <div className="Menu">
+        <div className="Funcionalidades">
+          <button className="BotaoMenu">USUARIO</button>
+          <button className="BotaoMenu">CONTAS CURTIDAS</button>
+        </div>
 
-    </div>
+        {props.ListaDeMatch.map((match) => {
+          return (
+          <div className="ListaDeMatch">
+            <img src={match.photo} alt=""  className="ImgDaListaDeMatch"/>
+            <h4>{match.name}</h4>
+          </div>)
+        })}
+
+      </div>
     </>
   );
 }
