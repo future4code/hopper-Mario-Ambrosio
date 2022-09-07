3b6d432b9ef2273bce6c7a05cb5ec89f3fa3a34a
MaritoAmbrosio <mario_conduta@hotmail.com>
2022-09-07 03:14:47 -0300

    Atualização

---
+++ modulo5/introducao-sql/src/error.ts
@@ -0,0 +1,9 @@
+export const ListError = {
+  errorRequestInvalid: 400,
+  errorCredentials: 401,
+  errorUserNotAuthorized: 403,
+  errorRouteNotFound: 404,
+  errorRegisterExist: 409,
+  errorParameterInvalid: 422,
+  errorStatus: 500
+}
